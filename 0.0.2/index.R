b4ab8cedaffb5d077b24c245540210954c558561
greamko@gmail.com <admin@39513.simplecloud.club>
2016-03-22 15:06:27 +0300

    ets

--- 0.0.2/index.R
+++ 0.0.2/index.R
@@ -4,6 +4,7 @@ library("forecast")
 setContentType("application/json")
 
 tryCatch({
+
   output <- list()
   outputData <- list()
   errors <- list()
@@ -36,12 +37,23 @@ tryCatch({
   if(tolower(uriData[1]) == 'arima'){
 
   	if(!is.null(userData$factors)){
-  		if(!is.null(userData$sfactors)){
-  			model <- arima(tsIn, order = unlist(userData$factors), seasonal=list(order = unlist(userData$sfactors)))
-  		}
-  		else{
-  			model <- arima(tsIn, order = unlist(userData$factors))
-  		}
+  			model <- Arima(
+          tsIn, 
+          order = unlist(userData$factors), 
+          seasonal=list(order = unlist(userData$sfactors)),
+          xreg = userData$xreg,
+          include.mean = userData$include.mean,
+          include.drift = userData$include.drift,
+          include.constant = userData$include.constant,
+          xreg = userData$xreg,
+          lambda = userData$lambda,
+          transform.pars = userData$transform.pars,
+          fixed = userData$fixed,
+          init = userData$init,
+          method = userData$method,
+          optim.control = userData$optim.control,
+          kappa = userData$kappa,
+          )
   	}
   	else{    
 	    model <- auto.arima(tsIn)
@@ -49,12 +61,22 @@ tryCatch({
   }
   else if(tolower(uriData[1]) == 'ets'){
 
-    if(!is.null(userData$model)){
-      model <- ets(tsIn, model = userData$model)
-    }
-    else{
-      model <- ets(tsIn)
-    }
+    model <- ets(
+      tsIn, 
+      model = userData$model, 
+      damped = userData$damped, 
+      alpha = userData$alpha, 
+      beta = userData$beta, 
+      gamma = userData$gamma, 
+      phi = userData$phi,
+      additive.only = userData$additive.only,
+      lower = userData$lower,
+      upper = userData$upper,
+      opt.crit  = userData$opt.crit,
+      nmse  = userData$nmse,
+      allow.multiplicative.trend  = userData$allow.multiplicative.trend
+      )
+    
   }
   else{
     stop("method not found")
